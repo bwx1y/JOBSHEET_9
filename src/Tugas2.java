276c7ef080866df6f97277bd53153145637a8e8d
Nedy Nugroho <nedynugroho2007@gmail.com>
2025-11-08 14:01:02 +0700

    Tugas 2

---
+++ src/Tugas2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah pesanan: ");
+        int orderQuantity = sc.nextInt();
+
+        String[] order = new String[orderQuantity];
+        double[] price = new double[orderQuantity];
+
+        for (int i = 0; i < orderQuantity; i++) {
+            sc.nextLine();
+            System.out.print("Masukan Nama Pesanan: ");
+            order[i] = sc.nextLine();
+            System.out.print("Masukkan harga: ");
+            price[i] = sc.nextDouble();
+        }
+
+        double priceResult = Arrays.stream(price).sum();
+
+        System.out.println();
+        System.out.println("== Pesanan ==");
+        for (int i = 0; i < orderQuantity; i++) {
+            System.out.printf("%d. %s Rp. %,.2f %n", i+1, order[i], price[i]);
+        }
+        System.out.printf("Total Harga: Rp. %,.2f", priceResult);
+    }
+}
