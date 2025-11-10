0180b6e9762f8426da09f931981fdb7341760141
Nedy Nugroho <nedynugroho2007@gmail.com>
2025-11-10 21:49:53 +0700

    update Tugas 2

--- src/Tugas2.java
+++ src/Tugas2.java
@@ -10,6 +10,7 @@ public class Tugas2 {
 
         String[] order = new String[orderQuantity];
         double[] price = new double[orderQuantity];
+        double priceResult = 0;
 
         for (int i = 0; i < orderQuantity; i++) {
             sc.nextLine();
@@ -17,14 +18,14 @@ public class Tugas2 {
             order[i] = sc.nextLine();
             System.out.print("Masukkan harga: ");
             price[i] = sc.nextDouble();
-        }
 
-        double priceResult = Arrays.stream(price).sum();
+            priceResult += price[i];
+        }
 
         System.out.println();
         System.out.println("== Pesanan ==");
         for (int i = 0; i < orderQuantity; i++) {
-            System.out.printf("%d. %s Rp. %,.2f %n", i+1, order[i], price[i]);
+            System.out.printf("%d. %s Rp. %,.2f %n", i + 1, order[i], price[i]);
         }
         System.out.printf("Total Harga: Rp. %,.2f", priceResult);
     }
